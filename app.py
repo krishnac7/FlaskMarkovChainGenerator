4ab790330cc3d63e40bcfc017d24081e6d43f49a
Krishna Chaitanya S. Balaga <krishna@Krishnas-MacBook-Air.local>
2018-02-17 04:23:09 +0530

    make it better

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def hello():
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<body>{story}</body>
+<body><blockquote>{story}<blockquote></body></html>
     """.format(story=mc.generate(start, 500))
 
 if __name__ == "__main__":
