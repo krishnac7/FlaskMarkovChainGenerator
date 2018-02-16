973367d4be1b190f95fcb76764a9f0457ace07cb
Krishna Chaitanya S. Balaga <krishna@Krishnas-MacBook-Air.local>
2018-02-17 04:27:58 +0530

    make it better

--- app.py
+++ app.py
@@ -94,12 +94,20 @@ def hello():
 <head>
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<body><blockquote>{story}</blockquote></body></html>
+<body>
+<div class="container">
+  <blockquote class="blockquote">
+    <p>{story}</p>
+    <footer>Based on 2states</footer>
+  </blockquote>
+</div>
+
+</body>
     """.format(story=mc.generate(start, 500))
 
 if __name__ == "__main__":
