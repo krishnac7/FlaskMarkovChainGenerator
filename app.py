0e38806f91c0b40b6df403a0ef457e54f8e4e4cd
Krishna Chaitanya S. Balaga <krishna@Krishnas-MacBook-Air.local>
2018-02-17 04:20:27 +0530

    make it better

--- app.py
+++ app.py
@@ -89,10 +89,17 @@ def hello():
     # Get some text to start with
     start = in_text[pos:pos+order]
 
-    return """<html>
-    <head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body>
-    <h1></h1>
-    <center><p>{story}</p></center></body></html>
+    return """<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>{story}</body>
     """.format(story=mc.generate(start, 500))
 
 if __name__ == "__main__":
